dbd6167b537d810199536c07610577f2b1529e6b
yujaw <yujawmanandhar@gmail.com>
2025-02-08 14:52:23 -0500

    Few Changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors = require('cors')
 
 const client = require('./config/db')
 
@@ -8,6 +9,8 @@ const PORT = 4000
 
 app.use(express.json({}))
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "public", "views")))
 app.use('/', require('./routers/root'))
